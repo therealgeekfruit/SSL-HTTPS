38754b8ea92760cba2f3b8351f388c8d4004a754
Sudeep Rao <55048261+therealgeekfruit@users.noreply.github.com>
2021-12-21 12:14:44 +0530

    new

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express')
 const https = require('https')
 const path = require('path')
 const fs = require('fs')
-
 const app = express()
 
-app.use('/', (req, res, next) => {
-  res.send('Hello from SSL server')
+//Static files
+app.use(express.static('public'))
+app.use("/css", express.static(__dirname + 'public/css'))
+app.use("/icons", express.static(__dirname + 'public/icons'))
+
+//Views
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+app.get("", (req, res) => {
+  res.render("index")
 })
+
+// app.listen(3443, ()=> console.log("Server is running on 3443"))
 
 const sslServer = https.createServer(
   {
